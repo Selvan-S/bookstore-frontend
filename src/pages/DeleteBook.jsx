ba812aa5be68298b620ec39306d83cc1101e3bba
Selvan S <87435305+Selvan-S@users.noreply.github.com>
2023-11-16 17:28:47 +0530

    first commit

---
+++ src/pages/DeleteBook.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSnackbar } from "notistack";
+
+const DeleteBook = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [book, setBook] = useState({});
+  const { enqueueSnackbar } = useSnackbar();
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:5555/books/${id}`)
+      .then((response) => {
+        setBook(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:5555/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar("Book is Deleted successfully", { variant: "success" });
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("An error happened. Please Check console");
+        enqueueSnackbar('Error', {variant: 'error'})
+        console.log(error);
+      });
+  };
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? Spinner : ""}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+        <h3 className="text-2xl"></h3>
+        <div className="my-4">
+          <span className="text-2xl mr-4 text-gray-500">TITLE</span>
+          <span className="text-2xl">{book.title}</span>
+        </div>
+        <h3 className="text-xl">
+          Are you certain you want to delete this book?
+        </h3>
+
+        <button
+          className="p-4 bg-red-600 text-white m-8 w-full"
+          onClick={handleDeleteBook}
+        >
+          Yes, Delete it
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteBook;
