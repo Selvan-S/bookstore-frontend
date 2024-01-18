7f2c81e530ba12bf27d546e91703748b10edc621
Selvan S <87435305+Selvan-S@users.noreply.github.com>
2024-01-18 17:24:06 +0530

    login bug fixed

--- src/components/home/Login.jsx
+++ src/components/home/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const Login = (login) => {
@@ -8,6 +8,26 @@ const Login = (login) => {
   };
   const navigate = useNavigate();
   const [user, setUser] = useState(initialUserState);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [nameAlert, setNameAlert] = useState(true);
+  const [idAlert, setIdAlert] = useState(true);
+
+  useEffect(() => {
+    if (user.name) {
+      setNameAlert(false);
+      if (user.id) {
+        setIsDisabled(false);
+        setIdAlert(false);
+      } else {
+        setIdAlert(true);
+        setIsDisabled(true);
+      }
+    } else {
+      setIsDisabled(true);
+      setNameAlert(true);
+    }
+  }, [user]);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUser({ ...user, [name]: value });
@@ -20,6 +40,24 @@ const Login = (login) => {
   return (
     <div className="w-full max-w-xs mx-auto my-16">
       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        {nameAlert && (
+          <div
+            className="bg-red-100 border border-red-400 text-red-700 px-4 py-1 rounded relative mb-1"
+            role="alert"
+          >
+            <strong className="font-bold">Username </strong>
+            <span className="block sm:inline">required!</span>
+          </div>
+        )}
+        {idAlert && (
+          <div
+            className="bg-red-100 border border-red-400 text-red-700 px-4 py-1 rounded relative mb-1"
+            role="alert"
+          >
+            <strong className="font-bold">User ID </strong>
+            <span className="block sm:inline">required!</span>
+          </div>
+        )}
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
@@ -62,6 +100,7 @@ const Login = (login) => {
         <div className="flex items-center justify-center">
           <button
             onClick={userLogin}
+            disabled={isDisabled}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
           >
